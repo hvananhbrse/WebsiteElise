61dc55e45bf29627519f683b7bd0c548510722e2
Hoàng Vân Anh <vananhdepgai9299@gmail.com>
2024-06-02 18:02:15 +0900

    Test

---
+++ src/components/layout/header/index.jsx
@@ -0,0 +1,11 @@
+import About from "../about";
+const Header = () => {
+    return (
+        <div className="">
+            Header
+            <About/>
+            {/* > */}
+        </div>
+    )
+}
+export default Header;
