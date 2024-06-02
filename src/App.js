4c84b09d544074de46f9667976577d45fc56799d
Hoàng Vân Anh <vananhdepgai9299@gmail.com>
2024-06-02 17:59:12 +0900

    Push Code Website Elise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import './Scss/main.scss';
+import Home from './components/pages/home';
+import About from './components/pages/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import About from "../about";
+const Home = () => {
+    return (
+        <div className="">
+            Home
+            <About/>
+        </div>
+    )
+}
+export default Home;
